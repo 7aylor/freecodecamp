7a824e1414309a7ed126ef1029e0e42bd498e7f8
Taylor <tbuchhei@gmail.com>
2016-12-02 16:46:53 -0800

    Parsing JSON working

--- Weather App/js/index.js
+++ Weather App/js/index.js
@@ -1,19 +1,20 @@
-var lat = navigator.geolocation.getCurrentPosition(getLat);
+var url = "http://api.openweathermap.org/data/2.5/weather?"
+const API_KEY = "&appid=e76968a3086bb53d9a54f6c1b0fb8739"
 
-function getLat(pos){
-  return pos.coords.latitude;
-}
-
-var long = navigator.geolocation.getCurrentPosition(getLong);
-  
-function getLong(pos){
-  return pos.coords.longitude;
-}
-
-console.log(lat);
-
-$('#loc').append(function(){
-  
+$(function() {
+  //get coordinates from ipinfo
+  $.getJSON('http://ipinfo.io', function(data) {
+    var coords = data.loc;
+    var lat = coords.substr(0, coords.indexOf(','));
+    var long = coords.substr(coords.indexOf(',') + 1, coords.length);
+    
+    //build our url
+    url += "lat=" + lat + "&lon=" + long + "&appid" + API_KEY + "&units=imperial";
+    
+    //then get the JSON and parse it to use in the webpage
+    $.getJSON(url, function(wd){
+      console.log(wd);
+      $('#loc').html(wd.main.temp);
+    });
+  });
 });
-
-var url = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139"
