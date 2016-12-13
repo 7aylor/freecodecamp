346b04965297dcb079d1a4eb507b081e981469b5
Taylor <tbuchhei@gmail.com>
2016-12-12 22:40:15 -0800

    numbers write to screen

---
+++ Calculator/js/index.js
@@ -0,0 +1,47 @@
+var screenNum = "";
+
+
+$("#one").on("click", function(){
+  screenNum += "1";
+  $("#screen").text(screenNum);
+});
+$("#two").on("click", function(){
+  screenNum += "2";
+  $("#screen").text(screenNum);
+});
+$("#three").on("click", function(){
+  screenNum += "3";
+  $("#screen").text(screenNum);
+});
+$("#four").on("click", function(){
+  screenNum += "4";
+  $("#screen").text(screenNum);
+});
+$("#five").on("click", function(){
+  screenNum += "5";
+  $("#screen").text(screenNum);
+});
+$("#six").on("click", function(){
+  screenNum += "6";
+  $("#screen").text(screenNum);
+});
+$("#seven").on("click", function(){
+  screenNum += "7";
+  $("#screen").text(screenNum);
+});
+$("#eight").on("click", function(){
+  screenNum += "8";
+  $("#screen").text(screenNum);
+});
+$("#nine").on("click", function(){
+  screenNum += "9";
+  $("#screen").text(screenNum);
+});
+$("#zero").on("click", function(){
+  screenNum += "0";
+  $("#screen").text(screenNum);
+});
+$("#dot").on("click", function(){
+  screenNum += ".";
+  $("#screen").text(screenNum);
+});
