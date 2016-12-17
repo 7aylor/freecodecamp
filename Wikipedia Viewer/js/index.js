39f60a4afb4ae69590a1b6421646dde296b9e053
Taylor <tbuchhei@gmail.com>
2016-12-17 15:08:06 -0800

    Functional wikipedia viewer, just need to style

--- Wikipedia Viewer/js/index.js
+++ Wikipedia Viewer/js/index.js
@@ -1,14 +1,27 @@
 var userInput = "";
-const url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&callback=?&titles=";
+const url = "https://en.wikipedia.org/w/api.php?action=opensearch&prop=revisions&rvprop=content&format=json&callback=?&search=";
 var buildUrl = url;
+
 $('#searchbtn').click(function() {
+  //get userinput
   userInput = $('#searchbox').val();
+  
+  //replace spaces with %20 so url loads properly
   userInput = userInput.replace(/\s/g, '%20');
+  
+  //clears url in case multiple searches are performed
   buildUrl = url;
   buildUrl += userInput;
-  console.log(buildUrl);
-  $.getJSON(buildUrl, function(wd){
-    $('results').html(wd.query);
+  
+  $.getJSON(buildUrl, function(data){
+    var searchResultsNames = data[1];
+    var searchResultsDetails = data[2];
+    var searchResultsURL = data[3];
+    $("#results ul").empty();
+    for(var i = 0; i < 10; i++){
+      $('#results ul').append("<a href=\"" + searchResultsURL[i] + "\"  target=\"_blank\"><li><h3>" + searchResultsNames[i] + "</h3><p>" + searchResultsDetails[i] + "</li></a>");
+    }
+    
   });
   
 });
