603176374eefe63847af2351ca8ffd58994b1d09
Taylor <tbuchhei@gmail.com>
2016-12-17 15:58:03 -0800

    Finished wikipedia viewer

--- Wikipedia Viewer/js/index.js
+++ Wikipedia Viewer/js/index.js
@@ -2,28 +2,30 @@ var userInput = "";
 const url = "https://en.wikipedia.org/w/api.php?action=opensearch&prop=revisions&rvprop=content&format=json&callback=?&search=";
 var buildUrl = url;
 
-$('#searchbtn').click(function() {
-  //get userinput
-  userInput = $('#searchbox').val();
-  
-  //replace spaces with %20 so url loads properly
-  userInput = userInput.replace(/\s/g, '%20');
-  
-  //clears url in case multiple searches are performed
-  buildUrl = url;
-  buildUrl += userInput;
-  
-  $.getJSON(buildUrl, function(data){
-    var searchResultsNames = data[1];
-    var searchResultsDetails = data[2];
-    var searchResultsURL = data[3];
-    $("#results ul").empty();
-    for(var i = 0; i < 10; i++){
-      $('#results ul').append("<a href=\"" + searchResultsURL[i] + "\"  target=\"_blank\"><li><h3>" + searchResultsNames[i] + "</h3><p>" + searchResultsDetails[i] + "</li></a>");
-    }
-    
-  });
-  
+$('#searchbox').keypress(function (e) {
+  if (e.which == 13) {
+    e.preventDefault();
+    //get userinput
+    userInput = $('#searchbox').val();
+
+    //replace spaces with %20 so url loads properly
+    userInput = userInput.replace(/\s/g, '%20');
+
+    //clears url in case multiple searches are performed
+    buildUrl = url;
+    buildUrl += userInput;
+
+    $.getJSON(buildUrl, function(data){
+      var searchResultsNames = data[1];
+      var searchResultsDetails = data[2];
+      var searchResultsURL = data[3];
+      $("#results ul").empty();
+      for(var i = 0; i < searchResultsNames.length; i++){
+        $('#results ul').append("<a href=\"" + searchResultsURL[i] + "\"  target=\"_blank\"><li><h3>" + searchResultsNames[i] + "</h3><p>" + searchResultsDetails[i] + "</li></a>");
+      }
+
+    });
+  }
 });
 
 /*
