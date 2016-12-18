763ab3323b48f10875a1c50e8cad5fcb733b03ca
Taylor <tbuchhei@gmail.com>
2016-12-17 22:40:02 -0800

    cleaned up comments in wiki js file

--- Wikipedia Viewer/js/index.js
+++ Wikipedia Viewer/js/index.js
@@ -1,10 +1,14 @@
-var userInput = "";
+//our base url
 const url = "https://en.wikipedia.org/w/api.php?action=opensearch&prop=revisions&rvprop=content&format=json&callback=?&search=";
 var buildUrl = url;
 
+//when enter is pressed
 $('#searchbox').keypress(function (e) {
   if (e.which == 13) {
+    
+    //prevents page from going blank
     e.preventDefault();
+    
     //get userinput
     userInput = $('#searchbox').val();
 
@@ -14,32 +18,16 @@ $('#searchbox').keypress(function (e) {
     //clears url in case multiple searches are performed
     buildUrl = url;
     buildUrl += userInput;
-
+    
+    //gets the JSON from the link and builds the list items from the JSON
     $.getJSON(buildUrl, function(data){
       var searchResultsNames = data[1];
       var searchResultsDetails = data[2];
-      var searchResultsURL = data[3];
+      var searchResultsUrl = data[3];
       $("#results ul").empty();
       for(var i = 0; i < searchResultsNames.length; i++){
-        $('#results ul').append("<a href=\"" + searchResultsURL[i] + "\"  target=\"_blank\"><li><h3>" + searchResultsNames[i] + "</h3><p>" + searchResultsDetails[i] + "</li></a>");
+        $('#results ul').append("<a href=\"" + searchResultsUrl[i] + "\"  target=\"_blank\"><li><h3>" + searchResultsNames[i] + "</h3><p>" + searchResultsDetails[i] + "</li></a>");
       }
-
     });
   }
 });
-
-/*
-$('#searchbox').keypress(function (e) {
-  if (e.which == 13) {
-    userInput = $('#searchbox').val();
-  userInput = userInput.replace(/\s/g, '%20');
-  buildUrl = url;
-  buildUrl += userInput;
-  console.log(buildUrl);
-  $.getJSON(buildUrl, function(wd){
-    $('results').html(wd.query);
-  });
-    return false;    //<---- Add this line
-  }
-});
-*/
