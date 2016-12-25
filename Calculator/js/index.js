7e863bedba168d50e7bdd8f1caf34e5609746f39
Taylor <tbuchhei@gmail.com>
2016-12-24 18:36:02 -0800

    working on operators and chaining numbers

--- Calculator/js/index.js
+++ Calculator/js/index.js
@@ -1,71 +1,118 @@
-var screenNum = "";
+$(document).ready(function(){
 
+  //string displayed on screen
+  var screenNum = "";
+  
+  //numbers used for arithmetic
+  var num1 = 0;
+  var num2 = 0;
+  
+  //store the operator being used
+  var operator = "";
+  
+  //button click functionality for numbers
+  clickNum(  "#one", "1");
+  clickNum(  "#two", "2");
+  clickNum("#three", "3");
+  clickNum( "#four", "4");
+  clickNum( "#five", "5");
+  clickNum(  "#six", "6");
+  clickNum("#seven", "7");
+  clickNum("#eight", "8");
+  clickNum( "#nine", "9");
+  
+  clickOperator("#divide", decodeURI("%C3%B7"));
+  clickOperator("#times", decodeURI("%C3%97"));
+  clickOperator("#minus", "-");
+  clickOperator("#plus", "+");
+  
+  //change colors of buttons on press
+  changeButtonColorOnPress(".number", "#353535", "#5F5F5F");
+  changeButtonColorOnPress(".symbol", "#5F5F5F", "#353535");
+  changeButtonColorOnPress( ".clear", "#ff4d4d", "#D1424E");
 
-$("#one").click(function(){
-  screenNum += "1";
-  $("#screen").text(screenNum);
-});
-$("#two").click(function(){
-  screenNum += "2";
-  $("#screen").text(screenNum);
-});
-$("#three").click(function(){
-  screenNum += "3";
-  $("#screen").text(screenNum);
-});
-$("#four").click(function(){
-  screenNum += "4";
-  $("#screen").text(screenNum);
-});
-$("#five").click(function(){
-  screenNum += "5";
-  $("#screen").text(screenNum);
-});
-$("#six").click(function(){
-  screenNum += "6";
-  $("#screen").text(screenNum);
-});
-$("#seven").click(function(){
-  screenNum += "7";
-  $("#screen").text(screenNum);
-});
-$("#eight").click(function(){
-  screenNum += "8";
-  $("#screen").text(screenNum);
-});
-$("#nine").click(function(){
-  screenNum += "9";
-  $("#screen").text(screenNum);
-});
-$("#zero").click(function(){
-  screenNum += "0";
-  $("#screen").text(screenNum);
-});
-$("#dot").click(function(){
-  screenNum += ".";
-  $("#screen").text(screenNum);
-});
-
-$(".number")
-  .mousedown(function(){
-    $(this).css("background-color", "#353535");
-  })
-  .mouseup(function(){
-    $(this).css("background-color", "#5F5F5F");
+  //click zero functionality
+  $("#zero").click(function(){
+    if(!screenNum == ""){
+      screenNum += "0";
+      checkLength();
+    }
+  });
+  
+  //click decimal point functionality
+  $("#dot").click(function(){
+    if(!screenNum.includes(".")){
+      if(screenNum == "" || screenNum == "0"){
+        screenNum = "0";
+      }
+      screenNum += ".";
+      $("#screen").text(screenNum);
+    }
+  });
+  
+  //click all clear functionality
+  $("#ac").click(function(){
+    num1 = 0;
+    num2 = 0;
+    screenNum = "";
+    operator = "";
+    $("#screen").text(0);
+    $("#small-screen").text("");
+  });
+  
+  //click clear entry functionality
+  $("#ce").click(function(){
+    if(operator !== ""){
+      num2 = 0;
+      screenNum = "";
+    }
+    else{
+      num1 = 0;
+      num2 = 0;
+      screenNum = "";
+      operator = "";
+    }
+    $("#screen").text(0);
   });
 
-$(".symbol")
-  .mousedown(function(){
-    $(this).css("background-color", "#5F5F5F");
-  })
-  .mouseup(function(){
-    $(this).css("background-color", "#353535");
-  });
 
-$(".clear")
-  .mousedown(function(){
-    $(this).css("background-color", "#ff4d4d");
-  })
-  .mouseup(function(){
-    $(this).css("background-color", "#D1424E");
-  });
+  //when a number button is clicked, display it, or print error if screenNum is too long
+  function clickNum(selector, value){
+    $(selector).click(function(){
+      screenNum += value;
+      checkLength();
+    });
+  }
+  
+  //operator click functionality
+  function clickOperator(selector, value){
+    $(selector).click(function(){
+      operator = value;
+      num1 = Number(screenNum);
+      $("#small-screen").text(screenNum + " " + operator);
+      screenNum = "";
+    });
+  }
+  
+  //check the length of screenNum, print error if its too long
+  function checkLength(){
+    if(screenNum.length > 7){
+      screenNum = "";
+      $("#screen").text("ERROR");
+    }
+    else{
+      $("#screen").text(screenNum);
+    }
+  }
+  
+  //when a button is pressed, change the color. When its release, change back
+  function changeButtonColorOnPress(className, downColor, upColor){
+    $(className)
+    .mousedown(function(){
+      $(this).css("background-color", downColor);
+    })
+    .mouseup(function(){
+      $(this).css("background-color", upColor);
+    });
+  }
+});
