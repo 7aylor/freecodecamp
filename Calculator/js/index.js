802ca90b336a00af88dcc0faad4c840cb57154bc
Taylor <tbuchhei@gmail.com>
2017-01-01 16:16:53 -0800

    Revamped js for calc

--- Calculator/js/index.js
+++ Calculator/js/index.js
@@ -2,23 +2,8 @@ $(document).ready(function(){
 
   //string displayed on screen
   var screenNum = "";
-  
-  //numbers used for arithmetic
-  var num1 = "";
-  var num2 = "";
-  var result = "";
-  
-  //store the operator being used
-  var operator = "";
-  
-  //function to clear number values
-  function clearVars(){
-    num1 = "";
-    num2 = "";
-    result = "";
-    screenNum = "";
-    operator = "";
-  }
+  var mathExpression = "";
+  var smallScreenExpression = "";
   
   //button click functionality for numbers
   clickNum(  "#one", "1");
@@ -37,16 +22,17 @@ $(document).ready(function(){
   clickOperator("#plus", "+");
   
   clickEquals();
-  
-  //change colors of buttons on press
-  changeButtonColorOnPress(".number", "#353535", "#5F5F5F");
-  changeButtonColorOnPress(".symbol", "#5F5F5F", "#353535");
-  changeButtonColorOnPress( ".clear", "#ff4d4d", "#D1424E");
 
+  function clearVars(){
+    screenNum = "";
+    mathExpression = "";
+    smallScreenExpression = "";
+  }
+  
   //click zero functionality
   $("#zero").click(function(){
-    if(!screenNum == ""){
-      screenNum += "0";
+    if(screenNum !== ""){
+      screenNum += 0;
       checkLength();
     }
   });
@@ -56,109 +42,97 @@ $(document).ready(function(){
     if(!screenNum.includes(".")){
       if(screenNum == "" || screenNum == "0"){
         screenNum = "0";
+        mathExpression += "0";
+        smallScreenExpression += "0";
       }
       screenNum += ".";
-      $("#screen").text(screenNum);
+      mathExpression += ".";
+      smallScreenExpression += ".";
+      writeToScreens(screenNum, smallScreenExpression);
     }
   });
   
   //click all clear functionality
   $("#ac").click(function(){
     clearVars();
-    $("#screen").text("0");
-    $("#small-screen").text("");
+    writeToScreens("0", "");
   });
   
   //click clear entry functionality
   $("#ce").click(function(){
-    if(operator !== ""){
-      num2 = "";
+    if(mathExpression[mathExpress.length - 1] !== ""){
       screenNum = "";
     }
     else{
       clearVars();
     }
+    
     $("#screen").text("0");
   });
   
   //when a number button is clicked, display it, or print error if screenNum is too long
   function clickNum(selector, value){
-    console.log("Number clicked");
     $(selector).click(function(){
-      screenNum += value;
-      checkLength();
+      if(screenNum.length > 7){
+        clearVars();
+        writeToScreens("0", "digit limit met");
+      }
+      else{
+        screenNum += value;
+        mathExpression += value;
+        smallScreenExpression += value;
+        writeToScreens(screenNum, smallScreenExpression);
+      }
     });
   }
   
-  //operator click functionality
-  function clickOperator(selector, value){
-    $(selector).click(function(){
-      console.log("Operator clicked");
-      if(num2 === "" && num1 === ""){
-        operator = value;
-        num1 = Number(screenNum);
-        if(screenNum === ""){
-          screenNum = "0 " + operator;
-        }
-        else{
-          screenNum = screenNum + " " + operator; 
-        }
-        $("#small-screen").text(screenNum);
-        screenNum = "";  
-      }
-      else{
-        operator = value;
-        num2 = Number(screenNum);
-        clickEquals();
-        screenNum = result + " " + operator;
-        $("#small-screen").text(screenNum);
-        num1 = result;
-        num2 = "";
-        screenNum = "";
-      }
-    });
-  }
-
+  
   //when equals is clicked
   function clickEquals(){
     
     $("#equals").click(function(){
-      if(num2 === ""){
-        num2 = Number(screenNum);
-      }
-      console.log(operator);
-      console.log("Num1: " + num1);
-      console.log("Num2: " + num2);
-      if(operator === decodeURI("%C3%B7")){
-        result = num1 / num2;
-      }
-      if(operator === decodeURI("%C3%97")){
-        result = num1 * num2;
-      }
-      if(operator === "-"){
-        result = num1 - num2;
-      }
-      if(operator === "+"){
-        result = num1 + num2; 
-      }
-      console.log(result);
-      $("#screen").text(result);
-      $("#small-screen").text(num1 + " " + operator + " " + num2);
-      clearVars();
+      
+      
     });
   }
   
+  //operator click functionality
+  function clickOperator(selector, operator){
+    $(selector).click(function(){
+      if(mathExpression[mathExpression.length - 1] !== "+" &&
+         mathExpression[mathExpression.length - 1] !== "-" &&
+         mathExpression[mathExpression.length - 1] !== "*" &&
+         mathExpression[mathExpression.length - 1] !== "/" &&
+         mathExpression !== ""){
+        
+        if(operator === decodeURI("%C3%B7")){
+          mathExpression += "/";
+          smallScreenExpression += decodeURI("%C3%B7");
+        }
+        if(operator === decodeURI("%C3%97")){
+          mathExpression += "*";
+          smallScreenExpression += decodeURI("%C3%97");
+        }
+        if(operator === "-"){
+          mathExpression += "-";
+          smallScreenExpression += "-";
+        }
+        if(operator === "+"){
+          mathExpression += "+";
+          smallScreenExpression += "+";
+        }
+        writeToScreens(operator, smallScreenExpression);
+        screenNum = "";
+      }
+    });
+  }  
 
-  //check the length of screenNum, print error if its too long
-  function checkLength(){
-    if(screenNum.length > 7){
-      screenNum = "";
-      $("#screen").text("ERROR");
-    }
-    else{
-      $("#screen").text(screenNum);
-    }
-  }
+ /* 
+ 
+  //change colors of buttons on press
+  changeButtonColorOnPress(".number", "#353535", "#5F5F5F");
+  changeButtonColorOnPress(".symbol", "#5F5F5F", "#353535");
+  changeButtonColorOnPress( ".clear", "#ff4d4d", "#D1424E");
   
   //when a button is pressed, change the color. When its release, change back
   function changeButtonColorOnPress(className, downColor, upColor){
@@ -168,6 +142,28 @@ $(document).ready(function(){
     })
     .mouseup(function(){
       $(this).css("background-color", upColor);
+    })
+    .mouseout(function(){
+      $(this).css("background-color", upColor);
     });
   }
+*/
+  
+  function writeToScreens(bigScreen, smallScreen){
+    $("#screen").text(bigScreen);
+    $("#small-screen").text(smallScreen);
+  }
+
+  function lastDigitIsOperator(){
+    if(mathExpression[mathExpression.length - 1] === "+" ||
+           mathExpression[mathExpression.length - 1] === "-" ||
+           mathExpression[mathExpression.length - 1] === decodeURI("%C3%97") ||
+           mathExpression[mathExpression.length - 1] === decodeURI("%C3%B7")){
+      return true
+    }
+    else{
+      return false;
+    }
+  }
+  
 });
