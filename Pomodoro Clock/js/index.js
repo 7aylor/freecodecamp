cb3e2b54f66afddded4e5175551333fcf6b4c080
Taylor <tbuchhei@gmail.com>
2017-01-09 21:28:05 -0800

    counter is working

--- Pomodoro Clock/js/index.js
+++ Pomodoro Clock/js/index.js
@@ -1,17 +1,37 @@
+//minutes used for clock
 var totalMinutes = 25;
 var totalSeconds = totalMinutes * 60;
 var displaySeconds = "00";
 
 $(document).ready(function(){
-  startTimer();
-  function startTimer(){
-    while(totalSeconds > 0){
-      $("#time-remaining").text(totalMinutes + ":" + displaySeconds);
-      setInterval(1000);
-      console.log(totalSeconds);
-      totalSeconds--;
-      totalMinutes--;
-    }
+  $("#time-remaining").text(totalMinutes + ":" + displaySeconds);
+  if(totalSeconds > 0){
+    setInterval(startTimer, 1000);
+  }
+});
+
+//timeer for clock
+function startTimer(){
+  //get remainder of totalSeconds / 60
+  var remainder = totalSeconds % 60;
+  
+  //subtract a second from totalSeconds
+  totalSeconds--;
+  
+  //if we are at a 59 second mark, subtract a minute
+  if(remainder === 59){
+    totalMinutes--;
   }
   
-});
+  //if remainder is less than 10, add a 0 to the front of the remainder
+  if(remainder < 10){
+    displaySeconds = "0" + remainder
+  }
+  //otherwise, use the remainder
+  else{
+    displaySeconds = remainder;
+  }
+  
+  //update our clock
+  $("#time-remaining").text(totalMinutes + ":" + displaySeconds);
+}
