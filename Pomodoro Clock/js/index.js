e75249d51a5a85ddfc6ec0ea61d4dda23a1b2230
Taylor <tbuchhei@gmail.com>
2017-01-11 20:55:55 -0800

    Clock is working with start and stop

--- Pomodoro Clock/js/index.js
+++ Pomodoro Clock/js/index.js
@@ -3,7 +3,8 @@ var totalMinutes = 25;
 var breakLength = 5;
 var totalSeconds = totalMinutes * 60;
 var displaySeconds = "00";
-var play = "Start";
+var start = "Start";
+var interval;
 
 $(document).ready(function(){
   setInitNums();
@@ -28,32 +29,42 @@ $(document).ready(function(){
   $("#subtract-session").click(function(){
     if(totalMinutes > 1){
       totalMinutes--;  
-    }    
+    }
+    
     setInitNums();
   });
   
-  /*
-  if(totalSeconds > 0){
-    setInterval(startTimer, 1000);
-  }
-  
-  */
+  $("#start").click(function(){
+    if(start == "Start"){
+      start = "Stop";
+      $("#start").toggleClass("stop");
+      $("#start").toggleClass("start");
+      if(totalSeconds > 0){
+        interval = setInterval(startTimer, 1000);
+      }
+    }
+    else{
+      start = "Start";
+      $("#start").toggleClass("stop");
+      $("#start").toggleClass("start");
+      clearInterval(interval);
+    }
+    $("#start").text(start);
+  });
   
 });
 
 //timeer for clock
 function startTimer(){
+
   //get remainder of totalSeconds / 60
   var remainder = totalSeconds % 60;
-  
-  //subtract a second from totalSeconds
-  totalSeconds--;
-  
+
   //if we are at a 59 second mark, subtract a minute
   if(remainder === 59){
     totalMinutes--;
   }
-  
+
   //if remainder is less than 10, add a 0 to the front of the remainder
   if(remainder < 10){
     displaySeconds = "0" + remainder
@@ -62,14 +73,18 @@ function startTimer(){
   else{
     displaySeconds = remainder;
   }
-  
   //update our clock
   $("#time-remaining").text(totalMinutes + ":" + displaySeconds);
+  
+  //subtract a second from totalSeconds
+  totalSeconds--;
+  
 }
 
 function setInitNums(){
   $("#sessionnum").text(totalMinutes);
   $("#breaknum").text(breakLength);
   $("#time-remaining").text(totalMinutes + ":" + displaySeconds);
+  $("#start").text(start);
   totalSeconds = totalMinutes * 60;
 }
