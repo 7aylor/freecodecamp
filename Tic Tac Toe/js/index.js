47fc33d793a1676fc8dc52ef99121f14c7861d84
Taylor <tbuchhei@gmail.com>
2017-01-25 21:01:39 -0800

    changes screens

--- Tic Tac Toe/js/index.js
+++ Tic Tac Toe/js/index.js
@@ -1,6 +1,17 @@
 var o = "&#9675";
 var x = "&times";
+var team;
 
 window.onload = function(){
   
+  document.getElementById("x").onclick = function() {teamChoiceClicked("x")};
+  document.getElementById("o").onclick = function() {teamChoiceClicked("o")};
+  
+  //set the team and hide choice screen, show tic tac toe board
+  function teamChoiceClicked(choice){
+    team = choice;
+    document.getElementById("player-choice").style.display = "none";
+    document.getElementById("container").style.display = "block";
+  }
+
 }
